16d00e4c3a2a00e7771cc13cce4611889543e2c3
Javier Cardenas <glatorian@gmail.com>
2021-01-10 01:01:07 -0500

    JS almost works, much added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,92 +1,110 @@
-//$(document).ready(function() {
+
 // set DateTime using luxon
 var DateTime = luxon.DateTime;
 var dt = DateTime.local();
 // display current date without update in document
-$("#currentDay").text(dt.toLocaleString(DateTime.DATE_HUGE));
-//parse JSON to object
-let storedSchedule = JSON.parse(localStorage.getItem("storedSchedule"));
-//when pulled from localStorage, update array
-if (storedSchedule !== null) {
-scheduleArray = storedSchedule;
-} else {
-    scheduleArray = new Array(9);
-    scheduleArray[5] = "Sample Text Area";
-}
-
-
-//console.log(scheduleArray)
-
-//clear rows
-$("plannerContainer").empty();
-
-for (let hour = 9; hour <= 17; hour++) {
-    let index = hour - 9;
-    //construct rows
-    $("#rowDiv").addClass("row plannerRow").attr("hour-index", hour);
-
-    // time element
-    let $mdTimeDiv = $("<div>");
-    $mdTimeDiv.addClass("col-lg-2 justify-content-center");
-  
-    // create timeBox element (contains time)
-    const $timeSpn = $("<span>");
-    // can use this to get value
-    $timeSpn.attr("class","timeSpan");
-    
-    // format hours for display
-    let displayHour = 0;
-    let ampm = "";
-    if (hour > 12) { 
-      displayHour = hour - 12;
-      ampm = "p.m.";
+$(document).ready(function () {
+    $("#currentDay").text(dt.toLocaleString(DateTime.DATE_HUGE));
+    //parse JSON to object
+    let storedSchedule = JSON.parse(localStorage.getItem("storedSchedule"));
+    //when pulled from localStorage, update array
+    if (storedSchedule !== null) {
+        scheduleArray = storedSchedule;
     } else {
-      displayHour = hour;
-      ampm = "a.m.";
+        scheduleArray = new Array(9);
+        scheduleArray[5] = "Sample Text Area";
     }
-    
-    $timeSpn.text(displayHour + " " + ampm);
-    //$timeSpn.text("${displayHour} ${ampm}");
 
-    //place into time element
-    $("#rowDiv").append($mdTimeDiv);
-    $mdTimeDiv.append($timeSpn);
+    //clear rows
+    $("plannerContainer").empty();
 
-    //now the input bit of the row
-    let $blockSpan = $("<input>");
+    for (let hr = 9; hr <= 17; hr++) {
+        let index = hr - 9;
+        //construct rows
+        $("#rowDiv").addClass("row plannerRow").attr("hr-index", hr);
 
-    //add attributes using array/object
-    $blockSpan.attr("id", "input-${index}");
-    $blockSpan.attr({
-        "hour-index": index,
-        "type": "text",
-        "class": "hourlyBlock",
-    })
+        // time element
+        let $lgTimeDiv = $("<div>");
+        $lgTimeDiv.addClass("col-lg-1 hr");
 
-    $blockSpan.val(scheduleArray[index]);
+        // create timeBox element (contains time)
+        const $timeSpn = $("<span>");
+        // can use this to get value
+        $timeSpn.attr("class", "timeSpan");
 
-    //place in col
-    let $col9Div = $("<div>");
-    $col9Div.addClass("col-lg-9 justify-content-center");
+        // format hours for display
+        let displayHour = 0;
+        let ampm = "";
+        if (hr > 12) {
+            displayHour = hr - 12;
+            ampm = "p.m.";
+        } else {
+            displayHour = hr;
+            ampm = "a.m.";
+        }
 
-    $("#rowDiv").append($col9Div);
-    $col9Div.append($blockSpan);
-    //placed
+        $timeSpn.text(displayHour + " " + ampm);
+        //$timeSpn.text("${displayHour} ${ampm}");
 
-    let $col1Save = $("<div>");
-    $col1Save.addClass("col-lg-1 justify-content-center");
+        //place into time element
+        $("#rowDiv").append($lgTimeDiv);
+        $lgTimeDiv.append($timeSpn);
 
-    //save button col
-    let $sBtn = $("<i>");
-    $sBtn.attr({
-        "id": "input-${index}",
-        "save-id": index,
-        "class": "fas fa-save",
-    })
+        //now the input bit of the row
+        let $blockSpan = $("<input>");
 
-    $("#rowDiv").append($col1Save);
-    $col1Save.append($sBtn);
-    //
+        //add attributes using array/object
+        $blockSpan.attr("id", "input-${index}");
+        $blockSpan.attr({
+            "hr-index": index,
+            "type": "text",
+            "class": "time-block, form-control",
+        })
 
+        $blockSpan.val(scheduleArray[index]);
 
-}
+        //place in col
+        let $col10Div = $("<div>");
+        $col10Div.addClass("col-lg-10");
+
+        $("#rowDiv").append($col10Div);
+        $col10Div.append($blockSpan);
+        //placed
+
+        let $col1Save = $("<div>");
+        $col1Save.addClass("col-lg-1 saveBtn");
+
+        //save button col
+        let $sBtn = $("<i>");
+        $sBtn.attr({
+            "id": "saveid-${index}",
+            "save-id": index,
+            "class": "fas fa-save",
+        })
+
+        $("#rowDiv").append($col1Save);
+        $col1Save.append($sBtn);
+        //
+        //update color
+        function updateColor($col10Div, hr) {
+            // does not seem to work, fix later
+            if (hr < dt.hour) {
+                $col10Div.attr("class", "col-lg-10 past");
+            } else if (hr > dt.hour) {
+                $col10Div.attr("class", "col-lg-10 future");
+            } else {
+                $col10Div.attr("class", "col-lg-10 present");
+            }
+        }
+        //save
+        //onclick
+        $(document).on("click", "i", function (event) {
+            event.preventDefault();
+            let $index = $(this).attr("save-id");
+            let inputId = `#input-${$index}`;
+            let $value = $(inputId).val();
+            scheduleArray[$index] = $value;
+            localStorage.setItem("storedSchedule", JSON.stringify(scheduleArray));
+        })
+    }
+})
